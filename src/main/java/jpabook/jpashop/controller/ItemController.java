057e8c7a4308fd85a6c6c5c00802a0afeedf1f75
Kim mosoo <ahtn21@dbinc.co.kr>
2024-11-04 17:54:13 +0900

    수정

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -20,13 +20,13 @@ public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping(value = "/items/new")
+    @GetMapping("/items/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
 
-    @PostMapping(value = "/items/new")
+    @PostMapping("/items/new")
     public String create(BookForm form) {
 
         Book book = new Book();
@@ -37,26 +37,17 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
-        return "redirect:/items";
+        return "redirect:/";
     }
 
-    /**
-     * 상품 목록
-     */
-    @GetMapping(value = "/items")
+    @GetMapping("/items")
     public String list(Model model) {
-
         List<Item> items = itemService.findItems();
-
         model.addAttribute("items", items);
-
         return "items/itemList";
     }
 
-    /**
-     * 상품 수정 폼
-     */
-    @GetMapping(value = "/items/{itemId}/edit")
+    @GetMapping("items/{itemId}/edit")
     public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
         Book item = (Book) itemService.findOne(itemId);
 
@@ -69,35 +60,14 @@ public class ItemController {
         form.setIsbn(item.getIsbn());
 
         model.addAttribute("form", form);
-
         return "items/updateItemForm";
     }
 
-    /**
-     * 상품 수정
-     */
-/*    @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
 
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
 
-        return "redirect:/items";
-    }*/
-    /**
-     * 상품 수정, 권장 코드
-     */
-    @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form")
-    BookForm form) {
-        itemService.updateItem(itemId, form.getName(), form.getPrice(),
-                form.getStockQuantity());
         return "redirect:/items";
     }
 }
